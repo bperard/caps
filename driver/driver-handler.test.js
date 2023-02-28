bc383eb61aba29b65ac760e4fdd35a7d7f607ccc
Brandon Perard <Brandon.Perard@Gmail.com>
2023-02-28 01:19:59 -0800

    Added driver-handler tests

---
+++ driver/driver-handler.test.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const eventEmitter = require('../eventEmitter');
+const handler = require('./handler');
+
+console.log = jest.fn();
+
+describe('driver-handler', () => {
+  it('handler.pickup() Logs pickup event with payload.orderID', () => {
+    const payload = {
+      orderID: 42,
+    };
+    handler.pickup(payload);
+
+    expect(console.log).toHaveBeenCalledWith(`DRIVER: picked up ${payload.orderID}`);
+  });
+
+  it('handler.delivered() Logs pickup event with payload.orderID', () => {
+    const payload = {
+      orderID: 42,
+    };
+    handler.delivered(payload);
+
+    expect(console.log).toHaveBeenCalledWith(`DRIVER: delivered up ${payload.orderID}`);
+  });
+});
