a539a36bb72c83103109b4f46aeb4b3cfae4b1c2
Brandon Perard <Brandon.Perard@Gmail.com>
2023-03-02 18:10:33 -0800

    Added queue functionality

---
+++ clients/flower-vendor/index.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const handler = require('./handler');
+const { socket } = require('../socket');
+
+setInterval (() => {
+  console.log('-------------=|=|<| NEW_FLOWER_ORDER |>|=|=-------------');
+  const newOrder = handler.createOrder();
+  const order = {
+    queueType: newOrder.store,
+    ...newOrder,
+  };
+
+  socket.emit('PICKUP', order);
+}, 5000);
+
+socket.on('DELIVERED', handler.delivered);
